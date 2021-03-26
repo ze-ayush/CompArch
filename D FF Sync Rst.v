1135308076707a73ac143b602cfc2f9c4610fde3
ze-ayush <60290001+ze-ayush@users.noreply.github.com>
2021-03-26 09:31:57 +0530

    Add files via upload

---
+++ D FF Sync Rst.v
@@ -0,0 +1,36 @@
+module d_sync( output reg q, q1, 
+               input wire clk, in , rst );
+always @(posedge clk ) 
+begin
+    if (rst) 
+      q=0;
+    else
+      q=in;
+    q1=~q; 
+end
+endmodule
+
+module tb_dsync;
+    reg in,clk,rst;
+    wire out, out1;
+    initial begin
+		$dumpfile("new.vcd");
+		$dumpvars;
+	end
+    always begin
+        #2 clk = ~clk;
+    end
+    d_sync des( out , out1 , clk, in, rst);
+    initial begin
+        clk = 1'b0;
+        in=1;
+        #0 
+        rst = 1'b1;
+        #2 rst = 1'b0;
+            in=1;
+        #4 rst = 1'b1;
+            in=1;    
+
+        #8 $finish;
+    end
+endmodule
